a8910875feaf1ba7c370aa6253728c6675a54447
MasroorAijaz <masrooraijaz08@gmail.com>
2018-11-30 20:50:25 +0500

    Updates

---
+++ precache-manifest.b931a8631ed20b73b12018b9d9d71b50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86ed2965d1d7992f90f0",
+    "url": "/giphyapp/static/css/main.0c3b8067.chunk.css"
+  },
+  {
+    "revision": "86ed2965d1d7992f90f0",
+    "url": "/giphyapp/static/js/main.86ed2965.chunk.js"
+  },
+  {
+    "revision": "3f0b22993c54ddcf39f8",
+    "url": "/giphyapp/static/js/1.3f0b2299.chunk.js"
+  },
+  {
+    "revision": "125ae232b57c7a57ac80",
+    "url": "/giphyapp/static/js/runtime~main.125ae232.js"
+  },
+  {
+    "revision": "4c897f0783ce90ed30857e555cbb2588",
+    "url": "/giphyapp/index.html"
+  }
+];
